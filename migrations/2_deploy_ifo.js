de401b521dff3a967c1292b51e1d6f9d66fdc73f
ape-swap <ape.swap@hotmail.com>
2021-04-15 22:54:16 -0300

    chore: Add Bitfresh IAO

--- migrations/2_deploy_ifo.js
+++ migrations/2_deploy_ifo.js
@@ -3,16 +3,16 @@ const IFO = artifacts.require("IFO");
 module.exports = async (deployer) => {
   // const num = 50 * Math.pow(10, 18);
   // const numAsHex = "0x" + num.toString(16);
-  const lpToken = '0xf65c1c0478efde3c19b49ecbe7acc57bb6b1d713'; // BANANA/BNB
-  const offeringToken = '0x0491648c910ad2c1afaab733faf71d30313df7fc'; // JDI
-  const startBlock = '6206300';
-  const endBlock = '6207500';
-  const offeringAmount = '7000000000000000000000000';
-  const raisingAmount = '23333000000000000000000';
+  const stakingToken = '0xf65c1c0478efde3c19b49ecbe7acc57bb6b1d713'; // BANANA/BNB
+  const offeringToken = '0xa4f93159ce0a4b533b443c74b89967c60a5969f8'; // JDI
+  const startBlock = '6835020';
+  const endBlock = '6836230';
+  const offeringAmount = '100000000000000000000000000';
+  const raisingAmount = '22250000000000000000000';
   const adminAddress = '0xC9F40d1c8a84b8AeD12A241e7b99682Fb7A3FE84';
   await deployer.deploy(
     IFO,
-    lpToken,
+    stakingToken,
     offeringToken,
     startBlock,
     endBlock,
