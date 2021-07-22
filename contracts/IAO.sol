47c5d80fe3b9a0debaef688a3485af12eaa7b4ff
defifofum <defifofum0@gmail.com>
2021-07-22 10:27:50 -0400

    feat|refactor: Provide IAO vesting period functionality - The IAO now has "harvest periods" (currently 4) which are used as intervals where a user can harvest an equal share each period until their offering amount is fully obtained. This is done by providing a harvest period offset at initalization. - _endBlock is now _endBlockOffset in init function - Upgrade compiler version to 0.8.6 - Use openzeppelin contract imports

--- contracts/IAO.sol
+++ contracts/IAO.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
 
 /*
  * ApeSwapFinance
@@ -10,30 +11,31 @@ pragma solidity 0.6.12;
  * GitHub:          https://github.com/ApeSwapFinance
  */
 
-import "@pancakeswap/pancake-swap-lib/contracts/math/SafeMath.sol";
-import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/IBEP20.sol";
-import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol";
-import "@pancakeswap/pancake-swap-lib/contracts/utils/ReentrancyGuard.sol";
-import "@pancakeswap/pancake-swap-lib/contracts/proxy/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 
-contract IFOByProxy is ReentrancyGuard, Initializable {
-    using SafeMath for uint256;
-    using SafeBEP20 for IBEP20;
+contract IAO is ReentrancyGuard, Initializable {
+    using SafeERC20 for IERC20;
+
+    uint256 constant public HARVEST_PERIODS = 4; 
+    uint256[HARVEST_PERIODS] public harvestReleaseBlocks;
 
     // Info of each user.
     struct UserInfo {
         uint256 amount; // How many tokens the user has provided.
-        bool claimed; // default false
+        bool[HARVEST_PERIODS] claimed; // default false
     }
 
     // admin address
     address public adminAddress;
     // The raising token
-    IBEP20 public stakeToken;
+    IERC20 public stakeToken;
     // Flag if stake token is BNB
     bool public isBNBStaking;
     // The offering token
-    IBEP20 public offeringToken;
+    IERC20 public offeringToken;
     // The block number when IFO starts
     uint256 public startBlock;
     // The block number when IFO ends
@@ -50,8 +52,9 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
     mapping(address => UserInfo) public userInfo;
     // participators
     address[] public addressList;
+    // FIXME: Using this allocation resulted in an error last time. Need to dive deeper
     // allocation precision 
-    uint256 private ALLOCATION_PRECISION = 1e8;
+    uint256 private ALLOCATION_PRECISION = 1e18; // Try using 1e8? 
 
     event Deposit(address indexed user, uint256 amount);
     event Harvest(
@@ -62,11 +65,14 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
     event EmergencySweepWithdraw(address indexed receiver, address indexed token, uint256 balance);
 
 
+    // https://docs.soliditylang.org/en/v0.8.6/introduction-to-smart-contracts.html#storage-memory-and-the-stack
+    // Functions can take up to 16 arguments
     function initialize(
-      IBEP20 _stakeToken,
-      IBEP20 _offeringToken,
+      IERC20 _stakeToken,
+      IERC20 _offeringToken,
       uint256 _startBlock,
-      uint256 _endBlock,
+      uint256 _endBlockOffset,
+      uint256 _vestingBlockOffset, // Block offset between vesting distributions
       uint256 _offeringAmount,
       uint256 _raisingAmount,
       address _adminAddress
@@ -78,7 +84,14 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
         }
         offeringToken = _offeringToken;
         startBlock = _startBlock;
-        endBlock = _endBlock;
+        endBlock = _startBlock + _endBlockOffset;
+        // Setup vesting release blocks
+        for (uint256 i = 0; i < HARVEST_PERIODS; i++) {
+            // https://blog.soliditylang.org/2020/12/16/solidity-v0.8.0-release-announcement/
+            harvestReleaseBlocks[i] = endBlock + (_vestingBlockOffset * i);
+        }
+
+
         offeringAmount = _offeringAmount;
         raisingAmount = _raisingAmount;
         totalAmount = 0;
@@ -114,7 +127,7 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
         depositInternal(msg.value);
     }
 
-    /// @dev Deposit BEP20 tokens with support for reflect tokens
+    /// @dev Deposit ERC20 tokens with support for reflect tokens
     function deposit(uint256 _amount) external onlyActiveIFO {
         require(!isBNBStaking, "stake token is BNB, deposit through 'depositBNB'");
         require(_amount > 0, "need _amount > 0");
@@ -124,42 +137,49 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
             address(this),
             _amount
         );
-        uint256 finalDepositAmount = getTotalStakeTokenBalance().sub(pre);
+        uint256 finalDepositAmount = getTotalStakeTokenBalance() - pre;
         depositInternal(finalDepositAmount);
     }
 
-    /// @notice To support BEP20 and BNB deposits this function does not transfer
+    /// @notice To support ERC20 and BNB deposits this function does not transfer
     ///  any tokens in, but only updates the state. Make sure to transfer in the funds
     ///  in a parent function
     function depositInternal(uint256 _amount) internal {
         if (userInfo[msg.sender].amount == 0) {
             addressList.push(address(msg.sender));
         }
-        userInfo[msg.sender].amount = userInfo[msg.sender].amount.add(
-            _amount
-        );
-        totalAmount = totalAmount.add(_amount);
-        totalDebt = totalDebt.add(_amount);
+        userInfo[msg.sender].amount = userInfo[msg.sender].amount + _amount;
+        totalAmount = totalAmount + _amount;
+        totalDebt = totalDebt + _amount;
         emit Deposit(msg.sender, _amount);
     }
 
-    function harvest() external nonReentrant {
+    function harvest(uint256 harvestPeriod) external nonReentrant {
+        require(harvestPeriod < HARVEST_PERIODS, "harvest period out of range");
+        // TODO: we should be able to remove the line below in favor of the line two lines below
         require(block.number > endBlock, "not harvest time");
+        require(block.number > harvestReleaseBlocks[harvestPeriod], "not harvest time");
         require(userInfo[msg.sender].amount > 0, "have you participated?");
-        require(!userInfo[msg.sender].claimed, "nothing to harvest");
-        uint256 offeringTokenAmount = getOfferingAmount(msg.sender);
+        require(!userInfo[msg.sender].claimed[harvestPeriod], "nothing to harvest");
+        // Refunds are only given on the first harvest
         uint256 refundingTokenAmount = getRefundingAmount(msg.sender);
-        offeringToken.safeTransfer(address(msg.sender), offeringTokenAmount);
         if (refundingTokenAmount > 0) {
             safeTransferStakeInternal(address(msg.sender), refundingTokenAmount);
         }
-        userInfo[msg.sender].claimed = true;
-        totalDebt = totalDebt.sub(userInfo[msg.sender].amount);
-        emit Harvest(msg.sender, offeringTokenAmount, refundingTokenAmount);
+    
+        uint256 offeringTokenAmountPerPeriod = getOfferingAmountPerPeriod(msg.sender);
+        offeringToken.safeTransfer(address(msg.sender), offeringTokenAmountPerPeriod);
+
+        userInfo[msg.sender].claimed[harvestPeriod] = true;
+        // Subtract user debt after refund on initial harvest
+        if(harvestPeriod == 0) {
+            totalDebt = totalDebt + userInfo[msg.sender].amount;
+        }
+        emit Harvest(msg.sender, offeringTokenAmountPerPeriod, refundingTokenAmount);
     }
 
-    function hasHarvest(address _user) external view returns (bool) {
-        return userInfo[_user].claimed;
+    function hasHarvest(address _user, uint256 harvestPeriod) external view returns (bool) {
+        return userInfo[_user].claimed[harvestPeriod];
     }
 
     /**
@@ -169,14 +189,14 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
      * 1 = 0.000000 000000 000001 (0.000000 000000 0001%)
      */ 
     function getUserAllocation(address _user) public view returns (uint256) {
-        return userInfo[_user].amount.mul(ALLOCATION_PRECISION).div(totalAmount);
+        return userInfo[_user].amount * ALLOCATION_PRECISION / totalAmount;
     }
 
     function getTotalStakeTokenBalance() public view returns (uint256) {
         if(isBNBStaking) {
             return address(this).balance;
         } else {
-            // Return BEP20 balance
+            // Return ERC20 balance
             return stakeToken.balanceOf(address(this));
         }
     }
@@ -185,22 +205,45 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
     function getOfferingAmount(address _user) public view returns (uint256) {
         if (totalAmount > raisingAmount) {
             uint256 allocation = getUserAllocation(_user);
-            return offeringAmount.mul(allocation).div(ALLOCATION_PRECISION);
+            return (offeringAmount * allocation) / ALLOCATION_PRECISION;
         } else {
             // userInfo[_user] / (raisingAmount / offeringAmount)
-            return
-                userInfo[_user].amount.mul(offeringAmount).div(raisingAmount);
+            // TODO: Double check this math
+            return (userInfo[_user].amount * offeringAmount) / raisingAmount;
         }
+    }
+
+    // get the amount of IFO token you will get per harvest period
+    function getOfferingAmountPerPeriod(address _user) public view returns (uint256) {
+        return getOfferingAmount(_user) / HARVEST_PERIODS;
     }
 
     // get the amount of lp token you will be refunded
     function getRefundingAmount(address _user) public view returns (uint256) {
-        if (totalAmount <= raisingAmount) {
+        // Users are able to obtain their refund on the first harvest only
+        if (totalAmount <= raisingAmount || userInfo[msg.sender].claimed[0] == true) {
             return 0;
         }
         uint256 allocation = getUserAllocation(_user);
-        uint256 payAmount = raisingAmount.mul(allocation).div(ALLOCATION_PRECISION);
-        return userInfo[_user].amount.sub(payAmount);
+        uint256 payAmount = (raisingAmount * allocation) / ALLOCATION_PRECISION;
+        return userInfo[_user].amount - payAmount;
+    }
+
+    // get the amount of IFO token you will get per harvest period
+    function tokensAvailableForHarvest(address _user) public view returns (uint256 stakeTokenHarvest, uint256 offeringTokenHarvest) {
+        uint256 currentBlock = block.number;
+        stakeTokenHarvest = 0;
+        offeringTokenHarvest = 0;
+        if(currentBlock < endBlock) {
+            return (0,0);
+        }
+        stakeTokenHarvest = getRefundingAmount(_user);
+
+        for (uint256 i = HARVEST_PERIODS - 1; i >= 0; i--) {
+            if(currentBlock > harvestReleaseBlocks[i]) {
+                offeringTokenHarvest += getOfferingAmountPerPeriod(_user);
+            }
+        }
     }
 
     function getAddressListLength() external view returns (uint256) {
@@ -233,15 +276,15 @@ contract IFOByProxy is ReentrancyGuard, Initializable {
             (bool success, ) = _to.call{gas: 23000, value: _amount}("");
             require(success, "TransferHelper: BNB_TRANSFER_FAILED");
         } else {
-            // Transfer BEP20 to address
-            IBEP20(stakeToken).safeTransfer(_to, _amount);
+            // Transfer ERC20 to address
+            IERC20(stakeToken).safeTransfer(_to, _amount);
         }
     }
 
-    /// @notice A public function to sweep accidental BEP20 transfers to this contract. 
+    /// @notice A public function to sweep accidental ERC20 transfers to this contract. 
     ///   Tokens are sent to owner
-    /// @param token The address of the BEP20 token to sweep
-    function sweepToken(IBEP20 token) external onlyAdmin {
+    /// @param token The address of the ERC20 token to sweep
+    function sweepToken(IERC20 token) external onlyAdmin {
         require(address(token) != address(stakeToken), "can not sweep stake token");
         require(address(token) != address(offeringToken), "can not sweep offering token");
         uint256 balance = token.balanceOf(address(this));
--- contracts/MockBEP20.sol
+++ contracts/MockBEP20.sol
@@ -1,13 +1,14 @@
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
 
-import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract MockBEP20 is BEP20 {
+contract MockERC20 is ERC20 {
     constructor(
         string memory name,
         string memory symbol,
         uint256 supply
-    ) public BEP20(name, symbol) {
+    ) ERC20(name, symbol) {
         _mint(msg.sender, supply);
 
     }
