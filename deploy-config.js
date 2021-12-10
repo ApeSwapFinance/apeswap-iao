28ebd9d1eb3346feb205d23022f12d7890d19dc9
defifofum <defifofum0@gmail.com>
2021-12-10 11:00:15 -0500

    refactor: Use given accounts for deployment

--- deploy-config.js
+++ deploy-config.js
@@ -17,8 +17,8 @@ function getNetworkConfig(network, accounts) {
     } else if (['development'].includes(network)) {
         console.log(`Deploying with development config.`)
         return {
-            adminAddress: '0xC9F40d1c8a84b8AeD12A241e7b99682Fb7A3FE84',
-            proxyAdminAddress: '0xC9F40d1c8a84b8AeD12A241e7b99682Fb7A3FE84',
+            adminAddress: accounts[0],
+            proxyAdminAddress: accounts[1],
             rpcProvider: 'http://127.0.0.1:8545',
         }
     } else {
